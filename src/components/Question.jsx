200d4e075ed008cb5679b94401d6569d9edfe0c8
rasel-fswd <rasel.fswd@gmail.com>
2024-08-15 18:27:46 +0600

    Change border color teal-600 to teal-700

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -17,7 +17,7 @@ export default function Question({ question, answer, dispatch }) {
               'block text-gray-600 py-3 px-4 border border-gray-600 w-full rounded-full text-lg hover:bg-gray-600 hover:border-gray-700 hover:text-gray-200 disabled:cursor-not-allowed transition',
               hasAnswer
                 ? i === question.correctOption
-                  ? 'bg-teal-600 text-gray-200 hover:bg-teal-600 border-teal-600 hover:border-teal-700'
+                  ? 'bg-teal-600 text-gray-200 hover:bg-teal-600 border-teal-700 hover:border-teal-700'
                   : 'bg-amber-500 hover:bg-amber-500 border-amber-600 hover:border-amber-600 hover:text-gray-600'
                 : ''
             )}
